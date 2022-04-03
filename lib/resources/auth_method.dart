5005852604c92e174ae01fcc680aca59dd8d6bf2
naneps <ekaprasetya2244@gmail.com>
2022-04-03 22:28:01 +0700

    create auth method to sign up

--- lib/resources/auth_method.dart
+++ lib/resources/auth_method.dart
@@ -1,1 +1,49 @@
-class AuthMethods {}
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    required Uint16List file,
+  }) async {
+    String res = "Some error occured";
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty ||
+          file != null) {
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        print(cred.user?.uid);
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'username': username,
+          'uid': cred.user!.uid,
+          'email': email,
+          'bio': bio,
+          'followers': [],
+          'following': []
+        });
+        // await _firestore.collection('users').add({
+        //   'username': username,
+        //   'uid': cred.user!.uid,
+        //   'email': email,
+        //   'bio': bio,
+        // });
+        res = "success";
+      }
+    } catch (e) {
+      res == e.toString();
+    }
+    return res;
+  }
+}
