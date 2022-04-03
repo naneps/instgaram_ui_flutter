c5398a4cd6f8de3e9896f365ab932310f255bfa2
naneps <ekaprasetya2244@gmail.com>
2022-04-03 19:52:04 +0700

    configuratinon firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:instgaram_ui_flutter/responsive/mobile_screen_layout.dart';
 import 'package:instgaram_ui_flutter/responsive/responsive_layout_screen.dart';
 import 'package:instgaram_ui_flutter/responsive/web_screen_layout.dart';
 import 'package:instgaram_ui_flutter/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
